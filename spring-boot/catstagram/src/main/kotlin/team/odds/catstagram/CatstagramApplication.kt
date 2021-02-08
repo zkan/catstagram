d80c39cbc4b9c30302f7d6f4d8c35d4d2dffa8e1
zkan <zkan.cs@gmail.com>
2021-02-08 22:41:08 +0700

    Create Spring Boot project :bear:

---
+++ spring-boot/catstagram/src/main/kotlin/team/odds/catstagram/CatstagramApplication.kt
@@ -0,0 +1,11 @@
+package team.odds.catstagram
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CatstagramApplication
+
+fun main(args: Array<String>) {
+	runApplication<CatstagramApplication>(*args)
+}
---
+++ spring-boot/catstagram/src/test/kotlin/team/odds/catstagram/CatstagramApplicationTests.kt
@@ -0,0 +1,13 @@
+package team.odds.catstagram
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CatstagramApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
