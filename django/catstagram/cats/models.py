bd785485ccfff8fa08f81f62b1d7ca6a9c2af853
zkan <zkan.cs@gmail.com>
2021-02-13 21:59:28 +0700

    Add image field to cat model :bear:

--- django/catstagram/cats/models.py
+++ django/catstagram/cats/models.py
@@ -3,3 +3,4 @@ from django.db import models
 
 class Cat(models.Model):
     name = models.CharField(max_length=300)
+    image = models.ImageField(null=True, blank=True)
--- django/catstagram/cats/tests/test_models.py
+++ django/catstagram/cats/tests/test_models.py
@@ -1,3 +1,7 @@
+import os
+from unittest.mock import MagicMock
+
+from django.core.files import File
 from django.test import TestCase
 
 from ..models import Cat
@@ -5,5 +9,13 @@ from ..models import Cat
 
 class TestCat(TestCase):
     def test_cat_should_have_defined_fields(self):
-        cat = Cat.objects.create(name='Salee')
+        name = 'Salee'
+        image_mock = MagicMock(spec=File)
+        image_mock.name = 'test.png'
+
+        cat = Cat.objects.create(name=name, image=image_mock)
+
         assert cat.name == 'Salee'
+        assert cat.image.name == image_mock.name
+
+        os.remove(f'media/{image_mock.name}')
